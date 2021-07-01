b5e200af4e0cab7b7326087e8ccc60e0d3615548
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-01 14:49:06 -0400

    Update WebScript.js

--- WebScript.js
+++ WebScript.js
@@ -1,1 +1,10 @@
-require('./methods/src/window')
+const windowjs = require('./methods/src/window')
+
+
+
+//CREATE FUNCTIONS
+var create_window = function(){windowjs.create_window()}
+
+module.exports = {
+    create_window: create_window
+}
